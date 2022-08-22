4401daa25fe8d84fe7007f77b0a39bd728c3d3e4
Sergey-Alex <strotelevsergwyalex@gmail.com>
2022-08-23 00:11:37 +0300

    add HW13 edits

--- src/p2-homeworks/hw13/Request.tsx
+++ src/p2-homeworks/hw13/Request.tsx
@@ -5,22 +5,26 @@ import SuperCheckbox from "../h4/common/c3-SuperCheckbox/SuperCheckbox";
 
 const Request = () => {
 
-    const [state, setState] = useState(false);
+    const [state, setState] = useState<boolean>(false);
+    const [resultText, setResultText] = useState<string>('')
 
     const onClickHandler = async () => {
-
         await axios.post(`https://neko-cafe-back.herokuapp.com/auth/test`, {success: state})
-            .then((response) => console.log(response))
+            .then((response) => setResultText(response.data.info))
             .catch(error => {
-                console.log(...error)
+                console.log({...error})
                 console.log(error.response ? error.response.data.errorText : error.message);
+                setResultText(error.response.data.errorText)
             })
+
+
     }
 
     return (
         <div>
             <SuperCheckbox onChange={() => setState(!state)}/>
             <Button onClick={onClickHandler}>btn</Button>
+            <span>{resultText}</span>
         </div>
     );
 };
